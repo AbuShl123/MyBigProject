8c30f22559e6848677c7895eeaa656554f6b640d
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-08-23 19:22:25 +0500

    completing challenge class for mentoring

--- src/main/java/CYDEO/mentoring/mentoring2308/Challenge.java
+++ src/main/java/CYDEO/mentoring/mentoring2308/Challenge.java
@@ -1,105 +1,57 @@
 package CYDEO.mentoring.mentoring2308;
 
-import java.util.*;
-
 public class Challenge {
+    // [2, 8, 5, 7]
 
-    public static int warOfNumbers(int[] numbers) {
-        int ev = 0;
-        int od = 0;
-        for (int i : numbers) {
-            if (i%2 == 0) ev += i;
-            else od += i;
-        }
-        return Math.abs(ev - od);
-    }
+    // 2+8 = 10
+    // 5 + 7 = 12
+    // --> 12-10 = 2
 
-    public static int countLetter(char[][] arr, char letter) {
-        ArrayList<Character> list = new ArrayList<>();
-        for (char[] v : arr) {
-            for (char e : v) {
-                list.add(e);
+    // return the difference between the sum of the even and odd numbers
+    /*
+    warOfNumbers([12, 90, 75]) ➞ 27
+    // 90 + 12 = 102
+    // 75
+    // 102 - 75 = 27
+
+    warOfNumbers([5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243]) ➞ 168
+     */
+    public static int warOfNumbers(int[] arr) {
+        int even = 0; // will be the sum even numbers
+        int odd = 0;
+
+        for (int each : arr) {
+            if (each % 2 == 0) {
+                even += each;
+            } else {
+                odd += each;
             }
         }
-        list.removeIf(e -> !e.equals(letter));
-        return list.size();
-    }
 
-    public static int[][] makeSquare(int n) {
-        int[][] arr = new int[n][n];
-        /*
-         [
-          [], [], [], [], []
-         ]
-         */
-        for (int[] v : arr) {
-            Arrays.fill(v, n);
-        }
-        return arr;
-    }
+        return Math.abs(even - odd);
+        // even = 6
+        // odd = 9
+        // --> 3
 
-    public static String rps(String s1, String s2) {
-        if(s1.equals(s2)) return "TIE";
-        return "";
-    }
-
-    public static Object basicCalculator(int a, char o, int b) {
-        int result;
-        switch (o) {
-            case '+' :
-                result = a + b; break;
-            case '-' :
-                result = a - b; break;
-            case '*' :
-                result = a * b; break;
-            case '/' :
-                if (b != 0) {
-                    result = a / b;
-                    break;
-                }
-            default:
-                return null;
-        }
-        return result;
-    }
-
-    public static boolean validateEmail(String s) {
-        return s.indexOf('@') > 0 && s.indexOf('.') > s.indexOf('@');
-    }
-
-    public static int spinAround(String[] turns) {
-        int a = 0;
-        for (String v : turns) {
-            if (v.equals("right"))
-                a += 90;
-            else
-                a+= -90;
-        }
-        return Math.abs(a/360);
+//        if (even > odd) return even - odd;
+//        else return odd - even;
     }
 
     public static void main(String[] args) {
-        int[] a = new int[10];
-        System.out.println(Arrays.toString(a));
-        System.out.println("---------------");
-        Arrays.fill(a, 8);
-        System.out.println(Arrays.toString(a));
+        System.out.println(warOfNumbers(new int[] {10, 5, 1, 2}));
+        // 10 + 2 = 12
+        // 5 + 1 = 6
+        // 12 - 6 = 6
 
+        System.out.println(warOfNumbers(new int[] {10, 5, 7}));
+        // 5 + 7 = 12
+        // 10
+        // 12 - 10 = 2
+        // 10 - 12 = -2
 
-        int[][] arr = new int[5][5];
-        System.out.println(Arrays.deepToString(arr));
-        for (int[] v : arr) {
-            Arrays.fill(v, 5);
-        }
-        for (int[] t : makeSquare(3)) {
-            System.out.println(Arrays.toString(t));
-        }
-
-
-        System.out.println(basicCalculator(120, '/', 0));
-
-        System.out.println(validateEmail("abu@gmail.com"));
-
-        System.out.println(spinAround(new String[] {"left", "left", "left", "left"}));
+        int a = -12;
+        System.out.println(a);
+        System.out.println(Math.abs(a));
+        // Math.abs always returns the positive number - absolute value of number
     }
 }

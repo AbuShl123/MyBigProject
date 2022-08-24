e11f70078822353e11e535f1720a13b6b1d921e6
Abubakr Shoaliev <shoalievabubakr@gmail.com>
2022-08-25 00:34:46 +0500

    Learning new Calendar class, also added some methods from HelloWorld class to my Meth class which will always store mathematical methods

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,85 +1,32 @@
+import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class HelloWorld {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-    public static int solveExample(String[] numbers) {
-        int answer = 0;
-        for (String each : numbers) {
-            answer += Integer.parseInt(each);
-        }
-        return answer;
-    }
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-    public static int solveExample(String str) {
-        String[] numbers = getNumbers(str);
-        int answer = 0;
-        for (String each : numbers) {
-            answer += Integer.parseInt(each);
-        }
-        return answer;
-    }
+        int month = Integer.parseInt(firstMultipleInput[0]);
 
-    public static String[] getNumbers(String example) { // example: "12-10+5-2+30"
-        String result = "";
-        for (int i = 0; i < example.length(); i++) {
-            char c = example.charAt(i);
-            if (c == '=') {
-                if (example.charAt(i+1) == '-') {
-                    result += " -";
-                    i++;
-                    continue;
-                }
-                result += " "; continue;
-            }
-            if (c == '+') {
-                result += " "; continue;
-            } else if ( c == '-') {
-                result += " ";
-            }
-            result += c;
-        }
-        return result.trim().split(" ");
-    }
+        int day = Integer.parseInt(firstMultipleInput[1]);
 
-    public static int solveEquation(String str) { // -5+x=15
-        String formula = getEquationFormula(str);
-        String[] n = getNumbers(formula.substring(formula.indexOf('=')+1));
+        int year = Integer.parseInt(firstMultipleInput[2]);
 
-        int x = solveExample(n);
-        return formula.startsWith("-") ? x*(-1) : x;
-    }
+        String res = Result.findDay(month, day, year);
 
-    public static String getEquationFormula(String str) { // str: -5 + x = 15 --> x = 15 +5
-        String[] parts = getNumbers(str);
-        String x;
-        String v;
-        if (parts[0].equals("x") || parts[0].equals("-x")) {
-            x = parts[0];
-            v = parts[1];
-        } else  {
-            x = parts[1];
-            v = parts[0];
-        }
-        if (v.contains("-")) v = "+" + v.substring(1); // -5 --> +5
-        else v = "-" + v;
-        return x + "=" + parts[2] + v;
-    }
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
 
-    public static void main(String[] args) {
-        System.out.println(solveExample(new String[]{"10", "-2", "9", "-4", "-7", "7"}));
-
-
-        String example = "12-10+5-2+30"; // 35
-        System.out.println(solveExample(example));
-
-        System.out.println(Arrays.toString(getNumbers("-23+x=-20")));
-        System.out.println(getEquationFormula("-23+x=-20"));
-        System.out.println(solveEquation("-23+x=-20"));
-
-        // some changes in HelloWorld class //
-
-        // some another changes //
+        bufferedReader.close();
+        bufferedWriter.close();
     }
 }
 
+class Result {
+    public static String findDay(int month, int day, int year) {
+        return "";
+    }
+}
--- src/main/java/MyOwn/mathOperators/Meth.java
+++ src/main/java/MyOwn/mathOperators/Meth.java
@@ -2,6 +2,7 @@ package MyOwn.mathOperators;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Meth {
     public String solvingBrackets(String str) { // str =  (23-3)+(32-1)+(12+4+110)-(23+44) = 20+31+136-67 = 0
@@ -43,5 +44,68 @@ public class Meth {
     public static double solveMath(String str) {
         Meth obj = new Meth();
         return obj.mathOpWithArr(obj.findNum(obj.solvingBrackets(str)));
+    }
+
+    public static int solveExample(String[] numbers) {
+        int answer = 0;
+        for (String each : numbers) {
+            answer += Integer.parseInt(each);
+        }
+        return answer;
+    }
+
+    public static int solveExample(String str) {
+        String[] numbers = getNumbers(str);
+        int answer = 0;
+        for (String each : numbers) {
+            answer += Integer.parseInt(each);
+        }
+        return answer;
+    }
+
+    public static String[] getNumbers(String example) { // example: "12-10+5-2+30"
+        String result = "";
+        for (int i = 0; i < example.length(); i++) {
+            char c = example.charAt(i);
+            if (c == '=') {
+                if (example.charAt(i+1) == '-') {
+                    result += " -";
+                    i++;
+                    continue;
+                }
+                result += " "; continue;
+            }
+            if (c == '+') {
+                result += " "; continue;
+            } else if ( c == '-') {
+                result += " ";
+            }
+            result += c;
+        }
+        return result.trim().split(" ");
+    }
+
+    public static int solveEquation(String str) { // -5+x=15
+        String formula = getEquationFormula(str);
+        String[] n = getNumbers(formula.substring(formula.indexOf('=')+1));
+
+        int x = solveExample(n);
+        return formula.startsWith("-") ? x*(-1) : x;
+    }
+
+    public static String getEquationFormula(String str) { // str: -5 + x = 15 --> x = 15 +5
+        String[] parts = getNumbers(str);
+        String x;
+        String v;
+        if (parts[0].equals("x") || parts[0].equals("-x")) {
+            x = parts[0];
+            v = parts[1];
+        } else  {
+            x = parts[1];
+            v = parts[0];
+        }
+        if (v.contains("-")) v = "+" + v.substring(1); // -5 --> +5
+        else v = "-" + v;
+        return x + "=" + parts[2] + v;
     }
 }
